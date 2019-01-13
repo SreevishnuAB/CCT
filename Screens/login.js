0e7863eae667bc63101907357cc2a0c1a493df16
SreevishnuAB <sreeab28@gmail.com>
2019-01-14 05:29:31 +0530

    implementing and testing axios

--- Screens/login.js
+++ Screens/login.js
@@ -4,6 +4,7 @@ import {TextField} from 'react-native-material-textfield';
 import styles from '../CSS/styles';
 import { observer } from 'mobx-react';
 import DataStore from '../Store/datastore';
+import axios from 'axios';
 
 @observer
 export default class LoginScreen extends React.Component{
@@ -16,11 +17,27 @@ export default class LoginScreen extends React.Component{
 		this.state = {user:'',pwd:'',verified:false};
 	}
 
+	_sendRequest = ()=>{
+		axios.post("https://c-c-t.herokuapp.com/php/login.php",
+			{
+				username:this.state.user,
+				password:this.state.pwd
+			}
+		)
+		.then(function(response){
+			console.log(response);
+		})
+		.catch(function(error){
+			console.log(error);
+		});
+	}
+
 	_onLogin = () => {
 //		TODO -- Axios backend comm implementation
 		if(this.state.user == '' || this.state.pwd == '')
 			alert("Username and password cannot be empty");
 		else /*if(this.state.verified)*/ {
+			this._sendRequest();
 			DataStore.updateUser(this.state.user);
 			this.setState({user:'',pwd:'',verified:false});
 			this.props.navigation.navigate('Barcode');
